b8b56d675e8951aeefa645e55276c5714941c00b
Dhriti <134310082+parikhdp@users.noreply.github.com>
2025-06-30 00:26:04 +0530

    Making the home page responsive

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
     <div className='flex p-4 px-10 md:px-32 lg-px-48 bg-primary border-b-2 border-black justify-between items-center'>
       <h2 className='font-bold text-lg bg-black text-white px-2 p-1'>Retro Store</h2>
 
-      <ul className='flex  gap-5 '>
+      <ul className='hidden md:flex gap-5 '>
         {MenuList.map((menu, index) => (
           <li key={menu.name} className='px-2 p-1 font-bold cursor-pointer hover:border-2 hover:vorder-white'>{menu.name} </li>
         ))}
--- app/_components/ProductCardItem.jsx
+++ app/_components/ProductCardItem.jsx
@@ -11,12 +11,12 @@ function ProductCardItem({ product }) {
                 <div className='mt-3'>
                     <h2 className='font-bold text-xl'>{product?.name}</h2>
                     <h2 className='font-bold text-2xl text-yellow-500'>₹ {product?.price}</h2>
-                    <div className='mt-3 flex justify-between items-center'>
+                    <div className='mt-3 md:flex justify-between items-center'>
                         <div className='flex items-center gap-2'>
                             <Image src={product?.user.image} alt={product?.user.name} width={30} height={30}  className='rounded-full'/>
                             <h2 className='text-sm text-gray-500'>{product?.user.name}</h2>
                         </div>
-                        <Button size='sm'>Add to Cart</Button>
+                        <Button size='sm' className='mt-1'>Add to Cart</Button>
                     </div>
                 </div>
             </Card>
